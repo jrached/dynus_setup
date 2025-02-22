e22f777b6bac1f23dbb713cf888f7f9fb5c39994
Harvey Merton <hmer101@mit.edu>
2025-02-22 01:53:11 +0000

    Added setup for lidar and mavros

--- start.sh
+++ start.sh
@@ -4,6 +4,16 @@
 source /opt/ros/humble/setup.bash
 source /root/code/zenoh_ws/install/setup.bash
 
+# Run setup files
+#chmod +x ./set_lidar_ip.sh
+./set_lidar_ip.sh
+cd /root/code/mavros_ws
+colcon build
+
+cd /root/code/livox_ws/src/livox_ros_driver2
+./build.sh humble
+#colcon build
+
 # Run zenoh bridge
 ros2 run zenoh_vendor zenoh-bridge-ros2dds -c /root/code/zenoh_ws/src/zenoh_vendor/configs/zenoh_dynus.json5
 
