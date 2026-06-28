fe446119fd3b5cd528470d0f8a0af74f97465e0a
jrached <91636826+jrached@users.noreply.github.com>
2026-06-28 15:02:50 -0400

    Clone 2025 Mavlink

--- setup_dynus_new.sh
+++ setup_dynus_new.sh
@@ -71,7 +71,7 @@ sudo apt-get install -y  ros-${ROS_DISTRO}-rqt-gui-py
 sudo apt-get install -y  ros-${ROS_DISTRO}-tf2-tools
 sudo apt-get install -y  ros-${ROS_DISTRO}-tf-transformations
 
-sudo apt install ros-${ROS_DISTRO}-mavlink
+# sudo apt install ros-${ROS_DISTRO}-mavlink
 sudo apt-get install -y  ros-${ROS_DISTRO}-desktop
 sudo apt-get install -y ros-dev-tools
 sudo apt-get install -y  ros-${ROS_DISTRO}-turtlesim
@@ -153,6 +153,8 @@ git clone https://github.com/jrached/direct_lidar_inertial_odometry.git
 mkdir -p ~/code/mavros_ws/src
 cd ~/code/mavros_ws/src
 git clone https://github.com/jrached/ros2_px4_stack.git
+cd ros2_px4_stack
+git switch dynus
 
 # trajgen_ws
 mkdir -p ~/code/trajgen_ws/src
@@ -166,6 +168,9 @@ git clone https://github.com/jrached/snapstack_msgs2.git
 mkdir -p ~/code/bridge_ws/src
 cd ~/code/bridge_ws/src
 git clone https://github.com/jrached/mavros.git
+git clone https://github.com/mavlink/mavlink-gbp-release.git -b upstream
+cd mavlink-gbp-release
+git checkout $(git rev-list -n 1 --before="2025-12-31" HEAD)
 
 # zenoh_ws
 mkdir -p ~/code/zenoh_ws/src
